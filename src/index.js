1f2f22d146955595cc4ead055a547423d5417555
Blackdoll-Loki <helpmeloki7@gmail.com>
2021-08-20 21:08:18 +0300

    solved task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
+  if(Array.isArray(array) && array.length !== 0){
+    return Math.min(...array);
+  }
   return 0;
 }
 
+
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length !== 0){
+    return Math.max(...array);
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && array.length !== 0){
+    let sum = array.reduce((acc,current) => acc + current,0);
+    return sum / array.length;
+  }
   return 0;
 }
